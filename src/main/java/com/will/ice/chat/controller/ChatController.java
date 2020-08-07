0351cbdb791995bb8915c7c0b526983fa4820e4d
Reizen3323 <66710242+Reizen3323@users.noreply.github.com>
2020-08-07 19:01:14 +0900

    ui단

--- src/main/java/com/will/ice/chat/controller/ChatController.java
+++ src/main/java/com/will/ice/chat/controller/ChatController.java
@@ -2,19 +2,33 @@ package com.will.ice.chat.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+@RequestMapping("/chat")
 public class ChatController {
 	
 	private Logger logger = LoggerFactory.getLogger(ChatController.class);
 	
-	@RequestMapping("/chat/chatMain")
+	@RequestMapping("/chat.do")
 	public void chatMain() {
 		
 	}
+
+	@RequestMapping("/chatDetail.do")
+	public void chatDetail() {
+		
+	}
+
+	@RequestMapping("/UI.do")
+	public void UI() {
+		
+	}
+
 	
-	@RequestMapping(value = "/chat/chatting", method = RequestMethod.GET)
+	@RequestMapping(value = "/chatting", method = RequestMethod.GET)
 	public void websocket() {
 		logger.info("채팅 방 보여주기");
 	}
